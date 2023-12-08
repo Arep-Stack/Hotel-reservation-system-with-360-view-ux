e8a4567be526bfa97d020949c2df71a12b895c11
Michael Isla <73521379+Mikee1301@users.noreply.github.com>
2023-12-09 01:10:41 +0800

    HRWV3V-000: Added cloudinary implementation (#5)
    Co-authored-by: Michael Isla <michael.isla@torocloud.com>

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,8 @@
+const cloudinaryConfig = {
+  cloud_name: 'deqdo01op',
+  api_key: '323449551644819',
+  api_secret: 'pB_eSKOfQuUi84D9L8Siwcxk4b4',
+  secure: true
+};
+
+module.exports = cloudinaryConfig;
--- routes/utils/ImageRoutes.js
+++ routes/utils/ImageRoutes.js
@@ -5,25 +5,9 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-// Specify the destination for multer to store the uploaded files
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const uploadFolder = 'uploads';
-    const uploadFolderPath = path.join(__dirname, '../../' ,uploadFolder);
-    // Create the upload folder if it doesn't exist
-    if (!fs.existsSync(uploadFolderPath)) {
-      fs.mkdirSync(uploadFolderPath);
-    }
-    cb(null, uploadFolderPath);
-  },
-  filename: function (req, file, cb) {
-    // Generate a unique filename for each uploaded file
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
-
+// Use memory storage for Multer
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 
 router.post('/image/upload', upload.single('image'), imageController.postImage);
 router.get('/image/:id', imageController.getImageById);
